cc1ae9c7ee1b3f9c30374cc6c17a528285141aaa
miiy <miiy@users.noreply.github.com>
2022-07-20 18:18:34 +0800

    Update

---
+++ spring/spring-springmvc/src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/user")
+    @ResponseBody
+    public String user() {
+        return "user";
+    }
+
+}
