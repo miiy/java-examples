f0b3c6da9c1a85bcca377d11710a00de71393577
miiy <miiy@users.noreply.github.com>
2022-07-21 10:25:50 +0800

    Update

--- spring/spring-springmvc-restful/src/main/java/org/example/config/ServletContainersInitConfig.java
+++ spring/spring-springmvc-restful/src/main/java/org/example/config/ServletContainersInitConfig.java
@@ -1,21 +1,17 @@
 package org.example.config;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
-    protected WebApplicationContext createServletApplicationContext() {
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(SpringMvcConfig.class);
-        return ctx;
+public class ServletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
     }
 
     protected String[] getServletMappings() {
         return new String[]{"/"};
-    }
-
-    protected WebApplicationContext createRootApplicationContext() {
-        return null;
     }
 }
